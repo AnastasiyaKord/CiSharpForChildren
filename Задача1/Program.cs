81321e2202d10fc364db3091fb410cea9c567e52
Anastasiya <vipstop2303@mail.ru>
2023-05-16 16:38:13 +0300

    задача1

---
+++ Задача1/Program.cs
@@ -0,0 +1,18 @@
+﻿Person Petr;
+Petr = new Person();
+Petr.firstName = "Petr";
+Petr.lastName = "Ivanov";
+Petr.ShowFullName();
+class Person
+{
+    // Поля
+    public string firstName;
+    public string lastName;
+    // Метод
+    public void ShowFullName()
+    {
+        Console.WriteLine("Name is " + firstName + " " + lastName);
+    }
+}
+
+
