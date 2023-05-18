1f1d118d6f730bd341dad8b22c32ea3285feb9c5
Anastasiya <vipstop2303@mail.ru>
2023-05-18 17:38:28 +0300

    .\Задача5\

---
+++ Задача5/Program.cs
@@ -0,0 +1,16 @@
+﻿Person Anna;
+Anna = new Person();
+int num = Anna.LuckyNumber(24, 14);
+Console.WriteLine("Счастливое число Анны: " + num);
+class Person
+{
+    // Поля
+    string firstName;
+    string lastName;
+    // Метод
+    int LuckyNumber (int numberOfTeeth, int age)
+    {
+        return (numberOfTeeth * age);
+    }
+}
+
